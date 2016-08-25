9bcf934f923fa5db3068cb9002cbc67ecc3771dd
Smay <smayphotography@gmail.com>
2016-08-25 23:14:28 +0100

    My first commit

---
+++ fpl.js
@@ -0,0 +1,24 @@
+var fplbot = require('./fplbot')
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// test route
+app.get('/', function (req, res) { res.status(200).send('Hello world!') });
+
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+
+app.listen(port, function () {
+  console.log('Slack bot listening on port ' + port);
+});
+
+app.post('/hello', fplbot);
